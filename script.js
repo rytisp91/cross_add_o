19f792502d39e0e8035f29e1d04f4f22261694ee
rytisp91 <68050841+rytisp91@users.noreply.github.com>
2020-11-25 16:13:50 +0200

    redone

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ let board = [
     "", "", "",
     "", "", ""
 ]
+let answerBoardX = []
+let answerBoardO = []
 let activePlayer = `X`
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 // FUNCTIONS
 
@@ -19,7 +31,7 @@ function generateBoxes() {
 
     mainContainer.innerHTML = ""
 
-    board.map((item, index)=>{
+    board.map((item, index) => {
         let smallBox = document.createElement('div')
         smallBox.classList.add(`smallerBox`)
         smallBox.setAttribute(`id`, index)
@@ -31,19 +43,49 @@ function generateBoxes() {
     })
 }
 
-function clickOnBox(event){
-    let index = event.target.id
-    if(activePlayer === `X`){
-        if(board[index] === ``){
+function clickOnBox(event) {
+    let index = Number(event.target.id)
+
+    if (activePlayer === `X`) {
+        if (board[index] === ``) {
             board[index] = `X`
+            answerBoardX.push(index)
             activePlayer = `O`
         }
     } else {
-        if(board[index] === ``) {
+        if (board[index] === ``) {
             board[index] = `O`
+            answerBoardO.push(index)
             activePlayer = `X`
         }
     }
 
     generateBoxes()
+    checkResult()
+    // console.log(answerBoardX, answerBoardO)
 }
+
+function checkResult() {
+    winningConditions.map(item => {
+        let a = item[0]
+        let b = item[1]
+        let c = item[2]
+        if (answerBoardX.includes(a) && answerBoardX.includes(b) && answerBoardX.includes(c)) {
+            alert(`X wins`)
+        }
+
+        if (answerBoardO.includes(a) && answerBoardO.includes(b) && answerBoardO.includes(c)) {
+            alert(`O wins`)
+        }
+    })
+
+}
+
+
+// XPlayerWin.map(item=>{
+//     console.log(item)
+// })
+
+// [0,1,2],
+// [3,4,5],
+// [6,7,8]
